421796a7421282e5cdc7ec62e409ed52239bd2a0
Blake Womack and Gloria Friesen <gloria@>
2017-05-04 09:37:42 -0700

    add new-task and edit-task components, add completeness filter, add ability to mark task as complete

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,28 +4,15 @@ import { Task } from './task.model';
 @Component({
   selector: 'app-root',
   template: `
-    <div class="container">
-      <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-      <h3>{{currentFocus}}</h3>
-      <task-list [childTaskList]="masterTaskList" (clickSender)="editTask($event)"></task-list>
-      <hr>
-      <div>
-        <div *ngIf="selectedTask">
-          <h3>{{selectedTask.description}}</h3>
-          <p>Task Complete? {{selectedTask.done}}</p>
-          <hr>
-          <h3>Edit Task</h3>
-          <label>Enter Task Description:</label>
-          <input [(ngModel)]="selectedTask.description">
-          <label>Enter Task Priority (1-3):</label><br>
-          <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
-          <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
-          <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
-          <button (click)="finishedEditing()">Done</button>
-        </div>
-      </div>
-    </div>
-  `
+  <div class="container">
+     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+     <h3>{{currentFocus}}</h3>
+     <task-list [childTaskList]="masterTaskList" (clickSender)="editTask($event)"></task-list>
+     <hr>
+     <edit-task [childSelectedTask]="selectedTask" (doneButtonClickedSender)="finishedEditing()"></edit-task>
+     <new-task (newTaskSender)="addTask($event)"></new-task>
+   </div>
+`
 })
 
 export class AppComponent {
@@ -48,5 +35,9 @@ export class AppComponent {
 
   finishedEditing() {
     this.selectedTask = null;
+  }
+
+  addTask(newTaskFromChild: Task) {
+    this.masterTaskList.push(newTaskFromChild);
   }
 }
